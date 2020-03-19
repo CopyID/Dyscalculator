beab0a252c9af5a6294f0ef109103bc4848acd33
Troy <Troyvandervelden@gmail.com>
2020-03-19 14:09:50 +0100

    Commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public float spin = 0.01666667f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,spin);
+    }
+}
